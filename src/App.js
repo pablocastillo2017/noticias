43fcc60e29b44d3d022f745c6cd6106e2ffd55b1
Pablo Castillo <pablocastilloastudillo@gmail.com>
2020-04-17 21:17:19 -0400

    Proy 11 -146. Colocando la categoría en el state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
 
 function App() {
+  // Definir Categoria y noticias desde el Submit
+
+  const [categoria, guardarCategoria] = useState("");
+
   return (
     <Fragment>
       <Header titulo="Buscador de Noticias" />
       <div className="container white">
         <h1>
-          <Formulario />
+          <Formulario guardarCategoria={guardarCategoria} />
         </h1>
       </div>
     </Fragment>
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./Formulario.module.css";
 import useSelect from "../hooks/useSelect";
 
-const Formulario = () => {
+const Formulario = ({ guardarCategoria }) => {
   const OPCIONES = [
     { value: "general", label: "General" },
     { value: "business", label: "Negocios" },
@@ -15,10 +15,17 @@ const Formulario = () => {
   //Urilizar el CUSTOM HOOK
   const [categoria, SelectNoticias] = useSelect("General", OPCIONES);
 
+  // submit al form , pasar categoria a App.js
+  const buscadorNoticias = (e) => {
+    e.preventDefault();
+
+    guardarCategoria(categoria);
+  };
+
   return (
     <div className={`${styles.buscador} row`}>
       <div className="col s12 m8 offset-m2">
-        <form>
+        <form onSubmit={buscadorNoticias}>
           <h2 className={styles.heading}>Encuentra Noticias por Categoría</h2>
 
           <SelectNoticias />
