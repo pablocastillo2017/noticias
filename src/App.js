74aa4e6482a81991627a4794b47574eaee1fe6e4
Pablo Castillo <pablocastilloastudillo@gmail.com>
2020-04-17 22:17:48 -0400

    Proy 11 -147. Consultando la API y leyendo los resultados

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
+// import axios from "axios";
 
 function App() {
   // Definir Categoria y noticias desde el Submit
 
   const [categoria, guardarCategoria] = useState("");
+
+  const [noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=us&category=${categoria}&apiKey=a4bf569189fa4617bb27cc8901a25e7d`;
+
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      guardarNoticias(noticias.articles);
+    };
+    consultarAPI();
+  }, [categoria]);
 
   return (
     <Fragment>
